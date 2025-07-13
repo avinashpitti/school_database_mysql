8439606767558ad15d50161328ed22df02040fbb
avinashpitti <avinash87129@gmail.com>
2025-07-13 23:00:26 +0530

    added some data and a table

--- employees_data.sql
+++ employees_data.sql
@@ -140,7 +140,81 @@ create view employee_summary as
 select first_name,last_name,phone_no from employees;
 select * from employee_summary;
 
-select * from employees; 
+create view pay as 
+select first_name,yearly_pay from employees;
+select * from pay;
+
+select * from employees;
+
+select * from employees where employee_id=5; 
+
+select * from employees where hourly_pay > 20;
+
+select first_name,hourly_pay from employees;
+
+select yearly_pay from employees;
+
+select yearly_pay from employees where yearly_pay between 34000 and 37500; # Both values are inclusive.
+
+alter table employees
+add constraint check_hourly_pay check(hourly_pay >10);
+
+insert into employees(employee_id,first_name,last_name,phone_no,hourly_pay,working_hours,hire_date)
+values
+(6,"amar","yadav","+91-9834278342",28.50,2000,"2023-01-07"); # if we give 8.5 instead of 28.5 it shows violation
+
+alter table employees
+modify column hire_date date after last_name; # Don't forget to add data type
+
+alter table employees
+modify column phone_no varchar(14) after yearly_pay;
+
+alter table employees
+modify column hire_date date after phone_no;
+
+select * from employees;
+
+
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO departments (department_id, department_name)
+VALUES 
+(1, 'HR'),
+(2, 'IT'),
+(3, 'Finance');
+
+
+select * from departments;
+
+ALTER TABLE employees
+ADD department_id INT;
+
+ALTER TABLE employees
+ADD CONSTRAINT fk_department
+FOREIGN KEY (department_id)
+REFERENCES departments(department_id);
+
+set sql_safe_updates=0;
+UPDATE employees
+SET department_id = CASE
+    WHEN employee_id = 1 THEN 1
+    WHEN employee_id = 2 THEN 2
+    WHEN employee_id = 3 THEN 3
+    WHEN employee_id = 4 THEN 1
+    WHEN employee_id = 5 THEN 2
+    WHEN employee_id = 6 THEN 3
+    WHEN employee_id = 7 THEN 1
+END;
+
+
+select * from employees;
+
+
+
+
 
 
 
